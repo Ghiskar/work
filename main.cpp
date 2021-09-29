6a460251a9948ba80a5568a397e84ba2ccae62ac
Zoe <zoe.klimenko@gmail.com>
2021-09-29 22:17:31 +0300

    added another way of calculation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+const double COEFFICIENT = 4/pow(3,2);
+
 int main(){
     // declaration
     int     R;
@@ -38,6 +40,26 @@ int main(){
     } else {
         (triangleArea > squareArea) ? printf("triangle area is the biggest\n")
                                     : printf("square area is the biggest\n");
+    }
+
+    // circleArea / triangleArea = 4/(3^(1/2)) * pi * (R/a)^2 = coef * pi * (R/a)^2
+    // circleArea / squareArea = pi * (R/d)^2
+    // squareArea / triangleArea = 4/(3^(1/2)) * (d/a)^2 = coef * (d/a)^2
+
+    double circleToTriangle = COEFFICIENT *M_PI * pow((R/a), 2);
+    double circleToSquare   = M_PI * pow((R/d), 2);
+    double squareToTriangle = COEFFICIENT * pow((d/a), 2);
+
+    printf("circleToTriangle = %lf\n",  circleToTriangle);
+    printf("circleToSquare = %lf\n",    circleToSquare);
+    printf("squareToTriangle = %lf\n",  squareToTriangle);
+
+    if (circleToTriangle > 1){
+        (circleToSquare > 1)    ? printf("circle area is the biggest\n")
+                                : printf("square area is the biggest\n");
+    } else {
+        (squareToTriangle > 1)  ? printf("square area is the biggest\n")
+                                : printf("triangle area is the biggest\n");
     }
 
     return 0;
