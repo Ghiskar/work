c04cec315f9c00eda72aca89064dabb47e65cd96
Zoe <zoe.klimenko@gmail.com>
2021-10-02 10:59:13 +0300

    error correction

--- main.cpp
+++ main.cpp
@@ -4,34 +4,32 @@
 const double COEFFICIENT = 4/pow(3,2);
 
 int main(){
-    // declaration
-    int     R;
-    int     a;
-    int     d;
-    double  circleArea;
-    double  triangleArea;
-    double  squareArea;
-
     // circle
+    int R;
+
     printf("Enter R\n");
     scanf("%d", &R);
 
-    circleArea      = M_PI * pow(R,2);
-    printf("circle area = %lf\n", circleArea);
+    double circleArea   = M_PI * pow(R,2);
+    printf("circle area = %f\n", circleArea);
 
     // triangle
+    int a;
+
     printf("Enter a\n");
     scanf("%d", &a);
 
-    triangleArea    = pow(a,2) * sqrt(3) / 4;
-    printf("triangle area = %lf\n", triangleArea);
+    double triangleArea = pow(a,2) * sqrt(3) / 4;
+    printf("triangle area = %f\n", triangleArea);
 
     // square
+    int d;
+
     printf("Enter d\n");
     scanf("%d", &d);
 
-    squareArea      = pow(d,2);
-    printf("square area = %lf\n", squareArea);
+    double squareArea   = pow(d,2);
+    printf("square area = %f\n", squareArea);
 
     // define, which area is bigger
     if (circleArea > triangleArea){
@@ -43,16 +41,17 @@ int main(){
     }
 
     // circleArea / triangleArea = 4/(3^(1/2)) * pi * (R/a)^2 = coef * pi * (R/a)^2
-    // circleArea / squareArea = pi * (R/d)^2
-    // squareArea / triangleArea = 4/(3^(1/2)) * (d/a)^2 = coef * (d/a)^2
-
     double circleToTriangle = COEFFICIENT *M_PI * pow((R/a), 2);
+
+    // circleArea / squareArea = pi * (R/d)^2
     double circleToSquare   = M_PI * pow((R/d), 2);
+
+    // squareArea / triangleArea = 4/(3^(1/2)) * (d/a)^2 = coef * (d/a)^2
     double squareToTriangle = COEFFICIENT * pow((d/a), 2);
 
-    printf("circleToTriangle = %lf\n",  circleToTriangle);
-    printf("circleToSquare = %lf\n",    circleToSquare);
-    printf("squareToTriangle = %lf\n",  squareToTriangle);
+    printf("circleToTriangle = %f\n",  circleToTriangle);
+    printf("circleToSquare = %f\n",    circleToSquare);
+    printf("squareToTriangle = %f\n",  squareToTriangle);
 
     if (circleToTriangle > 1){
         (circleToSquare > 1)    ? printf("circle area is the biggest\n")
@@ -61,6 +60,5 @@ int main(){
         (squareToTriangle > 1)  ? printf("square area is the biggest\n")
                                 : printf("triangle area is the biggest\n");
     }
-
     return 0;
 }
