1ab54964cc2acb046187b4b851626ff062628333
Zoe <zoe.klimenko@gmail.com>
2021-10-09 09:40:35 +0300

    fix, divide into two cicles

--- Task_3_5/main.cpp
+++ Task_3_5/main.cpp
@@ -15,32 +15,32 @@ int main(){
 
         if (x < 0.05 || x > 1){
             printf("wrong x\n");
-        } else {
-            double sum = 0;
-
-            while (true){
-                static double   f_1 = 1;
-                static double   f_2 = 1;
-                static double   tmp;
-                static int      n = 0;
-                static int      t = 2*n + 1;
-                static int      k = 1;
-
-                for (int i = k; i < t; ++i){
-                    (i % 2 != 0) ? (f_1 *= i) : (f_2 *= i);
-                }
-                tmp = f_1/f_2 * pow(x, t)/t;
-
-                if (tmp <= eps){ break; }
-
-                sum += tmp;
-                k = t;
-                n++;
-                t = 2*n + 1;
-            }
-            printf("Sum is %.18f\n", sum);
         }
     } while (x < 0.05 || x > 1);
+
+    double sum = 0;
+
+    while (true){
+        static double   f_1 = 1;
+        static double   f_2 = 1;
+        static double   tmp;
+        static int      n = 0;
+        static int      t = 2*n + 1;
+        static int      k = 1;
+
+        for (int i = k; i < t; ++i){
+            (i % 2 != 0) ? (f_1 *= i) : (f_2 *= i);
+        }
+        tmp = f_1/f_2 * pow(x, t)/t;
+
+        if (tmp <= eps){ break; }
+
+        sum += tmp;
+        k = t;
+        n++;
+        t = 2*n + 1;
+    }
+    printf("Sum is %.18f\n", sum);
 
     return 0;
 }
